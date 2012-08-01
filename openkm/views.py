d27e4731eea37a1edb15950c50fafdc89329966e
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2012-08-01 14:59:25 +0100

    Split up the get_document_by_uuid function
    Have split up the get_document_by_uuid function into get_document_by_uuid
    and get_document_buffer_by_uuid so that a StringIO buffer object can
    be retrieved.

--- openkm/views.py
+++ openkm/views.py
@@ -1,3 +1,4 @@
+import os
 import StringIO
 
 from django.http import HttpResponse
@@ -8,18 +9,30 @@ def get_document_by_uuid(request, uuid):
     """
     Returns a document from OpenKM
     """
+    # Get the file object and convert the string back to binary
+    file_obj, doc_meta = get_document_buffer_by_uuid(request, uuid)
+    document = utils.java_byte_array_to_binary(file_obj)
+    file_name = doc_meta.path.split("/")[-1]
+    file_name = os.path.basename(doc_meta.path)
+    print file_name
+    
+    # set the headers and return the file
+    response = HttpResponse(document, doc_meta.mimeType)
+    response['Content-Disposition'] = 'attachment; filename=%s' % file_name
+    return response
+
+def get_document_buffer_by_uuid(request, uuid):
+    """
+    Returns a document StringIO buffer and document meta data from OpenKM
+    """
     # get path from the uuid, and from that get the content
     document = client.Document()
     document_path = document.get_path(uuid)
     doc_meta = document.get_properties(document_path)
     java_byte_array = document.get_content(document_path, False)
-    
+  
     # convert the string back to binary
     file_obj = StringIO.StringIO(java_byte_array)
-    document = utils.java_byte_array_to_binary(file_obj)
-    
-    # set the headers and return the file
-    file_name = doc_meta.path.split("/")[-1]
-    response = HttpResponse(document, doc_meta.mimeType)
-    response['Content-Disposition'] = 'attachment; filename=%s' % file_name
-    return response
+
+    return file_obj, doc_meta
+
