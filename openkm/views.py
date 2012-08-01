8df8d28fb068a0e7771510a041b63b6a9aa41c84
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2012-08-01 16:32:02 +0100

    Small tidy up
    Did a quick test and everything apears to work so removed
    debugging lines.

--- openkm/views.py
+++ openkm/views.py
@@ -12,14 +12,13 @@ def get_document_by_uuid(request, uuid):
     # Get the file object and convert the string back to binary
     file_obj, doc_meta = get_document_buffer_by_uuid(uuid)
     document = utils.java_byte_array_to_binary(file_obj)
-    file_name = doc_meta.path.split("/")[-1]
     file_name = os.path.basename(doc_meta.path)
-    print file_name
     
     # set the headers and return the file
     response = HttpResponse(document, doc_meta.mimeType)
     response['Content-Disposition'] = 'attachment; filename=%s' % file_name
     return response
+
 
 def get_document_buffer_by_uuid(uuid):
     """
